07ff78e2e5b13ec276cc7212489ad4cb1e618cc0
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-08-09 12:10:17 +0530

    Tailwind Css

--- ToDoApp-Form/src/projects/Todo/Todo.jsx
+++ ToDoApp-Form/src/projects/Todo/Todo.jsx
@@ -2,11 +2,27 @@ import "./Todo.css";
 import {useState} from "react";
 export const Todo=()=>{
     const[inputValue , setInputValue] = useState(""); 
+
+    // Now When the user write some in input then to store thats content in an array we will have to need useState again , useState show below .
+
+    const[task , setTask] = useState([]);
+
+
+
     const handleInputChange=(value)=>{
         setInputValue(value);
     };
     const handleFormSubmit=(event)=>{
-        event.preventDefault(); // here preventDefault can prevent the the by default property of  form .
+        event.preventDefault();    // here preventDefault can prevent the the by default property of  form .
+
+        if(!inputValue) return;
+
+        // setTask((prevTask) => console.log(prevTask) ); // We can get previous value .
+
+        // ------------ , or , the just above code ---------------
+
+        setTask((prevTask) => [...prevTask , inputValue] ); // using spread operator       
+
     }
     return(
         <section className="todo-container">
