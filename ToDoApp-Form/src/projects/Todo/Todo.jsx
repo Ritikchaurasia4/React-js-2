f40204f26e12055acaa6f6a3fba633bf89217411
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-08-10 23:10:33 +0530

    Set date and time in Todo App form

--- ToDoApp-Form/src/projects/Todo/Todo.jsx
+++ ToDoApp-Form/src/projects/Todo/Todo.jsx
@@ -1,14 +1,19 @@
 import {useState} from "react";
 import { MdCheck, MdDeleteForever } from "react-icons/md";
+// import {useEffect } from "react";
 import "./Todo.css";
 export const Todo=()=>{
+
+    // Now When the user write some in input then to store thats content in an array we will have to need useState  , useState show below .
+    
+
     const[inputValue , setInputValue] = useState(""); 
 
     // Now When the user write some in input then to store thats content in an array we will have to need useState again , useState show below .
 
     const[task , setTask] = useState([]);
 
-
+    const[dateTime , setDateTime] = useState("");
 
     const handleInputChange = (value) => {
         setInputValue(value);
@@ -16,10 +21,10 @@ export const Todo=()=>{
     const handleFormSubmit = (event) =>{
         event.preventDefault();    // here preventDefault can prevent the the by default property of  form .
 
-        if(!inputValue) return; // first validation .
+        if(!inputValue) return; // first validation. Here do not store the data
 
-        if(task.includes(inputValue)){        // Second validation .
-            setInputValue("")
+        if(task.includes(inputValue)){    // Second validation . here if user  type any vlaue in an array then if that value already exist then return easily .
+            setInputValue("")  // It will be empty the input
             return ;
         } 
 
@@ -27,20 +32,69 @@ export const Todo=()=>{
 
         // ------------ , or , the just above code ---------------
 
-        setTask((prevTask) => [...prevTask , inputValue] ); // using spread operator  
+        setTask((prevTask) => [...prevTask , inputValue] ); // using spread operator We can put previous data as it 
         
-        setInputValue(""); // third validation .
+        setInputValue(""); // third validation . It will be empty the input 
 
     };
+
+    // ================ Todo , add date and time =======================
+
+
+    // const now = new Date();
+    // const formattedDate = now.toLocaleDateString();
+    // const formattedTime = now.toLocaleTimeString(); // Many time pages will be refresh to know tha date . Hence to write the below code through which date continuously changes
+
+     setInterval(() => {
+        const now = new Date();
+        const formattedDate = now.toLocaleDateString(); // If any data continuously change in react then we need useState.
+        const formattedTime = now.toLocaleTimeString();
+        setDateTime(`${formattedDate} - ${formattedTime}`);
+    } , 1000);
+
+    // ========== The just above code can also written as ..==== due to memory leakage problem ===================
+
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         const now = new Date();
+    //         const formattedDate = now.toLocaleDateString(); // If any data continuously change in react then we need useState.
+    //         const formattedTime = now.toLocaleTimeString();
+    //         setDateTime(`${formattedDate} - ${formattedTime}`);
+    //     } , 1000);
+
+    //     return () => clearInterval(interval);
+    
+    // } , []);
+
+    
+  
+    // ==================== the just Above code can also be written as ==========================================
+
+    // const getDateTime = () =>{
+    //      const now = new Date();
+    //      const formattedDate = now.toLocaleDateString();
+    //      const formattedTime = now.toLocaleTimeString();
+    // };
+
+    // setInterval(() => {
+    //     getDateTime();
+    // } , 1000);
+
+    // ==========================================================================================================
+
+
     return(
         <section className="todo-container">
             <header>
              <h1>Todo List</h1>
+            {/* <h1 className="date-time">{formattedDate}-{formattedTime}</h1> */}
+            <h1 className="date-time">{dateTime}</h1>
             </header>
+
             <section className="form">
                 <form onSubmit={handleFormSubmit}>
                     <div>
-                        <input type="text" className="to-input" autoComplete="off" value={inputValue} onChange={(event) => handleInputChange(event.target.value) }/>
+                        <input type="text" className="todo-input" autoComplete="off" value={inputValue} onChange={(event) => handleInputChange(event.target.value) }/>
                     </div>
                     <div>
                         <button type="submit" className="todo-btn">Add task</button>
