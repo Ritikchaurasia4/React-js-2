38184f7ea341a19491d5f48ddf5fdbd3afcb30b5
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-08-15 23:10:56 +0530

    Registration Form in React

--- AllHooks/src/App.jsx
+++ AllHooks/src/App.jsx
@@ -1,9 +1,11 @@
 // import {Counter} from "./hooks/useState/index";
 // import {CounterChallenge} from "./hooks/useState/challenge";
-import ControlledForm from "./hooks/useState/controlled";
+// import ControlledForm from "./hooks/useState/controlled";
+import {RegistrationForm} from "./hooks/useState/Registration";
 export const App = () =>{
   return (
         // <CounterChallenge/>
-           <ControlledForm/>
+        //  <ControlledForm/>
+           <RegistrationForm/>
   );
 };
