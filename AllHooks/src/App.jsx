7e5a60989416c780eb9a975f8feccb57785a3f99
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-08-17 20:56:17 +0530

    UseEffect

--- AllHooks/src/App.jsx
+++ AllHooks/src/App.jsx
@@ -1,11 +1,17 @@
 // import {Counter} from "./hooks/useState/index";
+
 // import {CounterChallenge} from "./hooks/useState/challenge";
+
 // import ControlledForm from "./hooks/useState/controlled";
-import {RegistrationForm} from "./hooks/useState/Registration";
+
+// import {RegistrationForm} from "./hooks/useState/Registration";
+
+import {ReactUseEffect} from "./hooks/useEffect/index";
 export const App = () =>{
   return (
         // <CounterChallenge/>
         //  <ControlledForm/>
-           <RegistrationForm/>
+       //  <RegistrationForm/>
+       <ReactUseEffect/>
   );
 };
