6976a3629829e9ec378e2e12372700c9b8468e44
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-08-17 20:55:35 +0530

    Date and Time using useEffect

---
+++ AllHooks/src/hooks/useEffect/index.jsx
@@ -0,0 +1,44 @@
+// import {useEffect, useState} from "react";
+// import "./index.css";
+
+// export const ReactUseEffect =() =>{
+//     const[count , setCount] = useState(0);
+
+//     useEffect(() =>{                               
+//         console.log("Count value:" , count);
+//     } ,[count]);
+
+//     return(
+//         <div className="container effect-container">
+//             <h1>  useEffect Hook</h1>
+//             <p>Count : {count}</p>
+//             <button onClick={() => setCount(count + 1)}> Increment </button>
+//         </div>
+//     );
+// };
+
+// ====================== for date and time ===========================
+
+// Since we know that , deals with the external data useEffect are used .
+
+
+import {useEffect, useState} from "react";
+import "./index.css";
+
+export const ReactUseEffect = () =>{
+    const[date , setDate] = useState(0);
+
+    useEffect(() => {
+        setInterval(() => {
+            const updatedDate = new Date();
+            setDate(updatedDate.toLocaleTimeString(date));
+        } , 1000);
+    } ,[]);
+
+    return(
+        <div className="container effect-container">
+            <h1> Date : {date} </h1>
+       
+        </div>
+    );
+};
