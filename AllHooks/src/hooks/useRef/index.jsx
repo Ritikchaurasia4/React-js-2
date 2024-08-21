0dcee96ec25c2dc6ee121c2a72863151829f9331
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-08-21 08:10:01 +0530

    useRef of Hook in react

---
+++ AllHooks/src/hooks/useRef/index.jsx
@@ -0,0 +1,28 @@
+import "./index.css";
+import {useRef} from "react";
+export const UseRef = () =>{
+
+    // const username = document.getElementById("username");
+    // const password = document.getElementById("password");
+
+    const username = useRef(null);
+    const password = useRef(null);
+
+    console.log(username);
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(username.current.value , password.current.value);    
+    }
+
+    return (
+        <form onSubmit={handleSubmit}> 
+
+            <input type="text" id="username" ref={username} />
+            <br />
+            <input type="text" id="password" ref={password} />
+            <br />
+            <button>Submit</button>
+        </form>
+    )
+};
