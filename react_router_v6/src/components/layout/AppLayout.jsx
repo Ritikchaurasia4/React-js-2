252feb74f407bf658e166cec605bb89c13080248
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-08-23 20:01:00 +0530

    AppLayout contain all pages

---
+++ react_router_v6/src/components/layout/AppLayout.jsx
@@ -0,0 +1,10 @@
+import { Footer } from "./Footer";
+import {Header} from "./Header";
+import {Outlet} from "react-router-dom";
+export const AppLayout = () =>{
+    return <>
+      <Header/>
+      <Outlet/>
+      <Footer/>
+    </>
+};
