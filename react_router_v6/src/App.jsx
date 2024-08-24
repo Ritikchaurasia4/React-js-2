bfa3244e0b21f5eefc75095304d664dfd048943f
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-08-24 06:15:19 +0530

    Basic of React router

--- react_router_v6/src/App.jsx
+++ react_router_v6/src/App.jsx
@@ -1,32 +1,153 @@
+// import {createBrowserRouter , RouterProvider , Route , createRoutesFromElements } from "react-router-dom";
+// import {Home} from "./pages/Home";
+// import {About} from "./pages/About";
+// import {Contact} from "./pages/Contact";
+// import {Movie} from "./pages/Movie";
+
+// export const App = () => {
+
+// // This below code is the old way to create route .
+
+// const router = createBrowserRouter(
+  
+// createRoutesFromElements(
+//   <Route>
+//       <Route path="/" element={<Home/>}/>
+//       <Route path="about" element={<About/>}/>
+//       <Route path="contact" element={<Contact/>}/>
+//       <Route path="movie" element={<Movie/>}/>
+//   </Route>
+// )
+
+// );
+
+//   return (
+//     <>
+//       <h1> Hello react Router </h1>
+//       <RouterProvider router={router}/>
+//     </>
+//   );
+// };
+
+
+
+// ============================= The just Above commented code can also be written as  , and We recommended the below code ===========================
+
+// import {createBrowserRouter , RouterProvider} from "react-router-dom";
+// import {Home} from "./pages/Home";
+// import {About} from "./pages/About";
+// import {Contact} from "./pages/Contact";
+// import {Movie} from "./pages/Movie";
+
+// export const App = () => {
+
+// const router = createBrowserRouter([
+//   {
+//     path : "/" ,
+//     element : <Home/>
+
+//   },
+//   {
+//     path : "/about" ,
+//     element : <About/>
+
+//   },
+//   {
+//     path : "/movie" ,
+//     element : <Movie/>
+
+//   },
+//   {
+//     path : "/contact" ,
+//     element : <Contact/>
+
+//   },
+// ]);
+
+//   return (
+//     <>
+//       <h1> Hello react Router </h1>
+//       <RouterProvider router={router}/>
+//     </>
+//   );
+// };
+
+// =========================================================================================================
+
 import {createBrowserRouter , RouterProvider} from "react-router-dom";
+
 import {Home} from "./pages/Home";
 import {About} from "./pages/About";
 import {Contact} from "./pages/Contact";
 import {Movie} from "./pages/Movie";
+import {AppLayout} from "./components/layout/AppLayout";
+
 
 export const App = () => {
 
 const router = createBrowserRouter([
-  {
-    path : "/" ,
-    element : <Home/>
 
-  },
-  {
-    path : "/about" ,
-    element : <About/>
+  
+  // {
+  //   path:"/" ,
+  //   element:<AppLayout/>
+  // } ,
 
-  },
-  {
-    path : "/movie" ,
-    element : <Movie/>
+  // {
+  //   path : "/" ,
+  //   element : <Home/>
 
-  },
-  {
-    path : "/contact" ,
-    element : <Contact/>
+  // },
+  // {
+  //   path : "/about" ,
+  //   element : <About/>
 
-  },
+  // },
+  // {
+  //   path : "/movie" ,
+  //   element : <Movie/>
+
+  // },
+  // {
+  //   path : "/contact" ,
+  //   element : <Contact/>
+
+  // },
+
+  // From the just above commented code , We we get one pages at a time only , But if we want to get all the pages inside the Header and Footer pages then for this the code is given below . Here we create a children object and all the code put inside the children . 👍
+
+
+  {
+    path:"/" ,
+    element:<AppLayout/>,
+
+    children : [
+
+        {
+          path : "/" ,
+          element : <Home/>
+      
+        },
+        {
+          path : "/about" ,
+          element : <About/>
+      
+        },
+        {
+          path : "/movie" ,
+          element : <Movie/>
+      
+        },
+        {
+          path : "/contact" ,
+          element : <Contact/>
+      
+        },
+      
+    ],
+    
+  } ,
+
 ]);
 
   return (
