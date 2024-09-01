4a2380a96ef4790189235c5c722a7041f3190727
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-09-01 17:43:21 +0530

    handling Error pages in react

--- react_router_v6/src/pages/ErrorPage.jsx
+++ react_router_v6/src/pages/ErrorPage.jsx
@@ -1,3 +1,41 @@
+// export const ErrorPage = () =>{
+//     return <h1>  404 Error page </h1>;
+// };
+
+// We can also handle the error using hook ,For this  the code are given below ......................
+
+
+import {useRouteError} from "react-router-dom";
+import { NavLink } from "react-router-dom"; 
+
 export const ErrorPage = () =>{
-    return <h1>  404 Error page </h1>;
+
+    const error = useRouteError();
+    console.log(error);
+    
+    if(error.status === 404){
+        return (
+            <section className="error-section">
+                <div id="error-text">
+                    <figure>
+                        <img src="https://cdn.dribbble.com/users/722246/screenshots/3066818/404-page.gif" alt="404 error" />
+                    </figure>
+                    <div className="text-center">
+                        <p className="p-a"> The page could not be found </p>
+                        <p className="p-b"> .. Back to Previous  page </p>
+                    </div>
+                </div>
+
+                <NavLink to="/" className="btn">
+                    Go Bak to Home page 
+                </NavLink>
+
+            </section>
+        );
+
+    }
+
+    
+
+    return <h1> The page you are looking does not exist </h1>;
 };
