22f7e6656f3a8e1053aadae0ffe3e1eaf4b74c75
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-09-03 21:24:31 +0530

    Create global loading state in React using react Router & useNavigation

--- react_router_v6/src/components/layout/AppLayout.jsx
+++ react_router_v6/src/components/layout/AppLayout.jsx
@@ -1,7 +1,16 @@
 import { Footer } from "./Footer";
 import {Header} from "./Header";
-import {Outlet} from "react-router-dom";
+import {Outlet , useNavigation} from "react-router-dom";
+
 export const AppLayout = () =>{
+
+  // The just below code i.e ( navigation ) is used to create loading , it means that whenever the actual data does not meet until we will show loading ...
+
+    const Navigation = useNavigation();
+    console.log(Navigation);
+
+    if(Navigation.state === "loading") return <Loading/> ;
+
     return <>
       <Header/>
       <Outlet/>
