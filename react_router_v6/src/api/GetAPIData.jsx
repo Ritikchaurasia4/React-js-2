c4ad8036ab32778180cf88757007dad6578b81ea
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-09-03 22:04:04 +0530

    Hide Key using a Variable

--- react_router_v6/src/api/GetAPIData.jsx
+++ react_router_v6/src/api/GetAPIData.jsx
@@ -1,6 +1,11 @@
 export const GetMoviesData = async() =>{
     try{
         const response = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=a64d7993&s=titanic&page=1");
+
+        // The just below commented code has written for hide the key ....
+
+        // const response = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${import.meta.env.VITE_API_KEY}&s=titanic&page=1`);
+
         const data = response.json();
         return data;
     } 
@@ -9,4 +14,4 @@ export const GetMoviesData = async() =>{
     }
 };
 
-// pass that above component/function name as the value of loader property in that page route
+// pass that above component/function name as the value of loader property in that page route .
